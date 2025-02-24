e90706a8453a87af7d0bf121fd799dd6fe6f3d86
taichi88 <nikoloz.gabeskiria@gmail.com>
2025-02-24 14:16:13 +0400

    2. Added Relevant folder for Architecture

---
+++ 1.solution/BankingSystem/BankingSystem/Controllers/IbankController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IbankController : ControllerBase
+    {
+    }
+}
