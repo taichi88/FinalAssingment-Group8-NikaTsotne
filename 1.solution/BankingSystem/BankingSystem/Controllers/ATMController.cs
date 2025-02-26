26561080f0d8a6fcc0754413b33952fab0d43512
taichi88 <nikoloz.gabeskiria@gmail.com>
2025-02-26 15:27:14 +0400

    test

--- 1.solution/BankingSystem/BankingSystem/Controllers/ATMController.cs
+++ 1.solution/BankingSystem/BankingSystem/Controllers/ATMController.cs
@@ -7,5 +7,29 @@ namespace BankingSystem.Controllers
     [ApiController]
     public class ATMController : ControllerBase
     {
+        //აქ უნდა დავაინჯექტო IATMService
+        public readonly IATMService _iatmService;
+
+
+        public ATMController(IATMService iATMService)
+        {
+            _iatmService = iATMService;
+        }
+
+        [HttpGet("authenticate")]
+        
+        public IActionResult Authenticate([FromBody] AuthenticationRequest request)
+
+        {
+            var authenticatedPerson = _atmService.Authenticate(request.CardNumber, request.PinCode);
+
+            if (authenticatedPerson == null)
+            {
+                return Unauthorized("Invalid credentials.");
+            }
+
+            return Ok(authenticatedPerson);
+        }
     }
+
 }
