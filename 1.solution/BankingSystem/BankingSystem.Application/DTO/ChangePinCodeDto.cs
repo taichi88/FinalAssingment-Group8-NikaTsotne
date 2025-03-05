7dc7f0ce5c920128c7a15d4ef4facc748d674988
taichi88 <nikoloz.gabeskiria@gmail.com>
2025-03-05 15:27:40 +0400

    updated controler

--- 1.solution/BankingSystem/BankingSystem.Application/DTO/ChangePinCodeDto.cs
+++ 1.solution/BankingSystem/BankingSystem.Application/DTO/ChangePinCodeDto.cs
@@ -4,5 +4,9 @@ namespace BankingSystem.Application.DTO;
 
 public class ChangePinCodeDto
 {
+    [Required(ErrorMessage = "Old PIN code is required")]
+    public string OldPinCode { get; set; }
 
+    [Required(ErrorMessage = "New PIN code is required")]
+    public string NewPinCode { get; set; }
 }
--- 1.solution/BankingSystem/BankingSystem.Application/DTO/WithdrawMoneyDto.cs
+++ 1.solution/BankingSystem/BankingSystem.Application/DTO/WithdrawMoneyDto.cs
@@ -5,5 +5,11 @@ namespace BankingSystem.Application.DTO;
 public class WithdrawMoneyDto
 {
 
+    [Required(ErrorMessage = "Amount is required")]
+    public decimal Amount { get; set; }
 
+    [Required(ErrorMessage = "Currency is required")]
+    public string Currency { get; set; }
+
+    public bool IsATM { get; set; } = true; // New property with default value
 }
