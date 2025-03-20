0a6d5ab546d1484cbe07253640f940b9e99a182a
Tsotne Gogoli <ts@gmail.com>
2025-03-21 02:22:28 +0400

    add service interface for transaction fee statistics.

---
+++ 1.solution/BankingSystem/BankingSystem.Application/IServices/IReportService.cs
@@ -0,0 +1,13 @@
+﻿using BankingSystem.Application.DTO.Response;
+using BankingSystem.Application.DTO.Response.ReportResponses;
+
+namespace BankingSystem.Application.IServices
+{
+    public interface IReportService
+    {
+        Task<UserStatisticsResponse> GetUserStatisticsAsync();
+        Task<TransactionStatisticsResponse> GetTransactionStatisticsAsync();
+        Task<MonthlyTransactionBreakdownResponse> GetMonthlyTransactionBreakdownAsync();
+        Task<AtmWithdrawalStatisticsResponse> GetAtmWithdrawalStatisticsAsync();
+    }
+}
