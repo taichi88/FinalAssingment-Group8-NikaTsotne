b1ed9d08c878390e79d2ccdd2049be27219808db
Tsotne Gogoli <ts@gmail.com>
2025-03-22 18:12:09 +0400

    rename Querry to Query

---
+++ 1.solution/BankingSystem/BankingSystem.Infrastructure/Query/Stored Procedures/AtmWithdrawalStatistics.sql
@@ -0,0 +1,11 @@
+-- ATM Withdrawal Statistics Stored Procedure
+CREATE OR ALTER PROCEDURE sp_GetTotalAtmWithdrawalAmount
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    SELECT ISNULL(SUM(Amount), 0) AS TotalWithdrawalAmount
+    FROM Transactions
+    WHERE IsATM = 1;
+END;
+GO
