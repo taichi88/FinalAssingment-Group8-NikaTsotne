9b8f5dab9fe6405bf4ee8272294c2ea463367157
Tsotne Gogoli <ts@gmail.com>
2025-03-26 22:11:25 +0400

    create application service registration in application layer

---
+++ 1.solution/BankingSystem/BankingSystem.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,23 @@
+using BankingSystem.Application.IServices;
+using BankingSystem.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BankingSystem.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // Register application services
+        services.AddScoped<IPersonAuthService, PersonAuthService>();
+        services.AddScoped<IAccountService, AccountService>();
+        services.AddScoped<ICardService, CardService>();
+        services.AddScoped<IAccountTransactionService, AccountTransactionService>();
+        services.AddScoped<IAtmService, AtmService>();
+        services.AddScoped<IPersonService, PersonService>();
+        services.AddScoped<IAtmTokenService, AtmTokenService>();
+        services.AddScoped<IReportService, ReportService>();
+
+        return services;
+    }
+}
