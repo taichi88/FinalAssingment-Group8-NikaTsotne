6eb06421f36873b76e9c31c0208cc137d156c2cb
Tsotne Gogoli <ts@gmail.com>
2025-03-26 22:12:50 +0400

    Program.cs refactor cleaner and small code

--- 1.solution/BankingSystem/BankingSystem/Program.cs
+++ 1.solution/BankingSystem/BankingSystem/Program.cs
@@ -1,81 +1,24 @@
-using BankingSystem.Infrastructure.Data.DatabaseContext;
-using System;
-using System.Data;
-using System.Text;
-using BankingSystem.Application.Identity;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using BankingSystem.Infrastructure;
+using BankingSystem.Application;
+using BankingSystem.Application.Helpers;
 using BankingSystem.Configure;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text.Json.Serialization;
+using BankingSystem.Domain;
+using BankingSystem.Infrastructure;
+using BankingSystem.Infrastructure.Data;
+using BankingSystem.Infrastructure.Data.DatabaseContext;
 using BankingSystem.Middleware;
 using Serilog;
-using Serilog.Events;
-using BankingSystem.Infrastructure.Data;
-using BankingSystem.Infrastructure.DataSeeding;
-using BankingSystem.Application.Helpers;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// Configure Seq
-builder.Host.UseSerilog((context, services, configuration) => configuration
-    .ReadFrom.Configuration(context.Configuration)
-    .ReadFrom.Services(services)
-    .Enrich.FromLogContext());
-
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
-
-builder.Services.AddDbContext<BankingSystemDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("BankingSystem.Infrastructure")));
-builder.Services.AddTransient<TestDataSeeder>();
-// Register the database initializer
-builder.Services.AddTransient<DatabaseInitializer>();
-
+builder.Services.AddApiServices(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
 
-var jwtKey = builder.Configuration["Jwt:Key"];
-var jwtIssuer = builder.Configuration["Jwt:Issuer"];
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        options.RequireHttpsMetadata = false;
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey!)),
-            ValidateIssuer = true,
-            ValidIssuer = jwtIssuer,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.FromMinutes(3)
-        };
-    });
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddScoped<IDbConnection>(_ =>
-    new SqlConnection(connectionString));
+// Configure Serilog
+builder.Host.UseSerilog((context, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .Enrich.FromLogContext());
 
 var app = builder.Build();
 
@@ -91,16 +34,13 @@ app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-// Initialize database before starting the application
-using (var scope = app.Services.CreateScope())
-{
-    var dbInitializer = scope.ServiceProvider.GetRequiredService<DatabaseInitializer>();
-    await dbInitializer.InitializeAsync();
-}
+// Initialize database using extension method from Infrastructure layer
+await app.Services.InitializeDatabaseAsync();
 
 app.Run();
