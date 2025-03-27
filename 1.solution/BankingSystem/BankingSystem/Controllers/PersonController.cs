9dbc0cbd2bee3a7b8fb58d9f216556a5128959c4
Tsotne Gogoli <ts@gmail.com>
2025-03-27 14:52:33 +0400

    Refactor PersonController for improved functionality
    Updated PersonController with model validation, API attributes, and authorization roles. Simplified TransferMoney method and changed its route. Added GetProfile method for user information retrieval, integrating functionality from the removed GetPersonInfo method.

--- 1.solution/BankingSystem/BankingSystem/Controllers/PersonController.cs
+++ 1.solution/BankingSystem/BankingSystem/Controllers/PersonController.cs
@@ -1,5 +1,4 @@
 ﻿using BankingSystem.Application.DTO;
-using BankingSystem.Application.DTO.Response;
 using BankingSystem.Application.IServices;
 using BankingSystem.Filters;
 using Microsoft.AspNetCore.Authorization;
@@ -7,34 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using BankingSystem.Middleware;
 
-namespace BankingSystem.Controllers
+namespace BankingSystem.Controllers;
+
+[ValidateModel]
+[ApiController]
+[Authorize(Roles = "Person")]
+[Route("api/[controller]")]
+public class PersonController(IAccountTransactionService transactionService, IPersonService personService) : ControllerBase
 {
-    [ValidateModel]
-    [ApiController]
-    [Route("api/[controller]")]
-    public class PersonController(IAccountTransactionService transactionService, IPersonService personService) : ControllerBase
+    [HttpPost("transactions")]
+    public async Task<IActionResult> CreateTransaction(TransactionDto transactionDto)
     {
-        [Authorize(Roles = "Person")]
-        [HttpPost("transfer-money")]
-        public async Task<IActionResult> TransferMoney(TransactionDto transactionDto)
-        {
-            var userId = User.FindFirst("userId")!.Value;
-            var result = await transactionService.TransactionBetweenAccountsAsync(transactionDto, userId);
+        var userId = User.FindFirst("userId")!.Value;
+        var result = await transactionService.TransactionBetweenAccountsAsync(transactionDto, userId);
+        return Ok(ErrorHandlingMiddleware.CreateSuccessResponse(HttpStatusCode.OK, result));
+    }
 
-            var response = ErrorHandlingMiddleware.CreateSuccessResponse(HttpStatusCode.OK, new { Message = result });
-            return Ok(response);
-        }
-
-        [Authorize(Roles = "Person")]
-        [HttpGet("get-info")]
-        public async Task<IActionResult> GetPersonInfo()
-        {
-            var userId = User.FindFirst("userId")!.Value;
-
-            var result = await personService.GetPersonById(userId);
-
-            var response = ErrorHandlingMiddleware.CreateSuccessResponse(HttpStatusCode.OK, result);
-            return Ok(response);
-        }
+    [HttpGet("profile")]
+    public async Task<IActionResult> GetProfile()
+    {
+        var userId = User.FindFirst("userId")!.Value;
+        var result = await personService.GetPersonById(userId);
+        return Ok(ErrorHandlingMiddleware.CreateSuccessResponse(HttpStatusCode.OK, result));
     }
 }
